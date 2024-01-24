d2feea94ba64ebb5a01214421dd9f49a17a10784
Gurjot Singh <gurjot12111@gmail.com>
2024-01-24 12:47:16 +0530

    Added sendFriendRequest, viewFriendRequest, take

--- controller/homePageController.js
+++ controller/homePageController.js
@@ -14,5 +14,75 @@ class homePageController {
         return res.status(500).json({ message: "Invalid User" });
       });
   };
+  static sendFriendRequest = async (req, res) => {
+    const { currentUserId, sentUserId } = req.body;
+    if (!currentUserId || !sentUserId) {
+      return res.status(500).json({ message: "Invalid User" });
+    }
+    console.log(currentUserId, sentUserId);
+    try {
+      await User.findByIdAndUpdate(sentUserId, {
+        $push: { friendRequests: currentUserId },
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Invalid User" });
+    }
+    try {
+      await User.findByIdAndUpdate(currentUserId, {
+        $push: { sentFriendRequests: sentUserId },
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Invalid User" });
+    }
+    return res
+      .status(200)
+      .json({ message: "Friend Request Sent", success: true });
+  };
+  static takeFriendRequest = async (req, res) => {
+    const { currentUserId, sentUserId } = req.body;
+    if (!currentUserId || !sentUserId) {
+      return res.status(500).json({ message: "Invalid User" });
+    }
+    console.log(currentUserId, sentUserId, "  ", "Hiii");
+    try {
+      await User.findByIdAndUpdate(currentUserId, {
+        $pull: { sentFriendRequests: sentUserId },
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Invalid User" });
+    }
+    try {
+      await User.findByIdAndUpdate(sentUserId, {
+        $pull: { friendRequests: currentUserId },
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Invalid User" });
+    }
+    return res
+      .status(200)
+      .json({ message: "Friend Request taken back", success: true });
+  };
+  static viewFriendRequests = async (req, res) => {
+    const userId = req.params.userId;
+    if (!userId) {
+      return res.status(500).json({ message: "Invalid User" });
+    }
+    try {
+      const user = await User.findOne({ _id: userId })
+        .populate("friendRequests", "name email image")
+        .lean();
+      const friendRequests = user.friendRequests;
+      return res
+        .status(200)
+        .json({ friendRequests: friendRequests, message: "Friend Requests" });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Invalid User" });
+    }
+  };
 }
 module.exports = homePageController;
--- routes/homePageRoutes.js
+++ routes/homePageRoutes.js
@@ -2,4 +2,10 @@ const express = require("express");
 const homePageController = require("../controller/homePageController");
 const homePageRouter = express.Router();
 homePageRouter.get("/getUsers/:userId", homePageController.getAllUsers);
+homePageRouter.post("/sendFriendRequest", homePageController.sendFriendRequest);
+homePageRouter.post("/takeFriendRequest", homePageController.takeFriendRequest);
+homePageRouter.get(
+  "/viewFriendRequests/:userId",
+  homePageController.viewFriendRequests
+);
 module.exports = homePageRouter;
